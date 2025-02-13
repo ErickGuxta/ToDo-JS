afa0c1cf6abbac05d5cf3620dec70e2d44ef9273
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-02-13 01:28:06 -0300

    .

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const taskForm = document.querySelector(".tasks-add")
 const taskInput = document.querySelector(".tasks-add input")
 const generalTasks = document.querySelector(".general-task-list")
+const generalTasksDone = document.querySelector(".general-task-list-done")
 
 //Funções
 const saveTodo = (text) => {
@@ -21,16 +22,22 @@ const saveTodo = (text) => {
     icons.classList.add("icons")
 
     const checkBtn = document.createElement("img")
-    const RemoveBtn = document.createElement("img")
+    checkBtn.classList.add("check-btn")
+    const removeBtn = document.createElement("img")
+    removeBtn.classList.add("remove-btn")
+
 
     checkBtn.src = "imgs/incon-check.svg"
-    RemoveBtn.src = "imgs/icon-lixo.svg"
+    removeBtn.src = "imgs/icon-lixo.svg"
 
     icons.appendChild(checkBtn)
-    icons.appendChild(RemoveBtn)
+    icons.appendChild(removeBtn)
     taskView.appendChild(icons)
 
     generalTasks.appendChild(taskView)
+
+    taskInput.value = ""//limpar input após o submit
+    taskInput.focus() //foca no input após o submit
 
 }
 //Eventos
@@ -39,8 +46,24 @@ taskForm.addEventListener("submit", (element) => {
     element.preventDefault() //não quero enviar o form par ao back, ja que to trabalhando com o front
     const inputValue = taskInput.value
     if (inputValue.trim() === "") {
-        alert("O campo não pode esta")
+        alert("O campo não pode esta vazio")
     } else{
         saveTodo(inputValue)
     }
 })
+
+
+document.addEventListener("click", (element) => {
+    const targetEl = element.target
+    const parentEl = targetEl.closest(".task-view")
+
+    if(!parentEl) retun //se não encontrar a tarefa evita erro
+
+    if (targetEl.classList.contains("check-btn")) {
+        generalTasksDone.appendChild(parentEl)
+    } 
+    
+    if (targetEl.classList.contains("remove-btn")) {
+        parentEl.remove()
+    }
+} )
