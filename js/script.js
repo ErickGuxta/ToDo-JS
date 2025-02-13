facdc967d1329f6224645f5d5ab993632549fe1d
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-02-13 00:20:24 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 //Seleção elementos
 const taskForm = document.querySelector(".tasks-add")
 const taskInput = document.querySelector(".tasks-add input")
+const generalTasks = document.querySelector(".general-task-list")
 
 //Funções
 const saveTodo = (text) => {
+
     //criando o template de add tasks
     const taskView = document.createElement("div")
     taskView.classList.add("task-view")
@@ -11,13 +13,24 @@ const saveTodo = (text) => {
     //criando texto interno
     const contentTask = document.createElement("p")
     contentTask.classList.add("content-task")
-
     contentTask.innerText = text
     taskView.appendChild(contentTask)
 
-    console.log(taskView)
+    //ciando botoes
+    const icons = document.createElement("div")
+    icons.classList.add("icons")
 
+    const checkBtn = document.createElement("img")
+    const RemoveBtn = document.createElement("img")
 
+    checkBtn.src = "imgs/incon-check.svg"
+    RemoveBtn.src = "imgs/icon-lixo.svg"
+
+    icons.appendChild(checkBtn)
+    icons.appendChild(RemoveBtn)
+    taskView.appendChild(icons)
+
+    generalTasks.appendChild(taskView)
 
 }
 //Eventos
@@ -25,8 +38,9 @@ const saveTodo = (text) => {
 taskForm.addEventListener("submit", (element) => {
     element.preventDefault() //não quero enviar o form par ao back, ja que to trabalhando com o front
     const inputValue = taskInput.value
-    if (inputValue) {
+    if (inputValue.trim() === "") {
+        alert("O campo não pode esta")
+    } else{
         saveTodo(inputValue)
-
     }
 })
