ac5ce6b253e86034658c41303495bd8fe0114de3
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-02-14 16:12:37 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ const taskForm = document.querySelector(".tasks-add")
 const taskInput = document.querySelector(".tasks-add input")
 const generalTasks = document.querySelector(".general-task-list")
 const generalTasksDone = document.querySelector(".general-task-list-done")
+const searchForm = document.querySelector(".tasks-search")
+const searchInput = document.querySelector(".tasks-search input")
+
 
 //Funções
 const saveTodo = (text) => {
-
     //criando o template de add tasks
     const taskView = document.createElement("div")
     taskView.classList.add("task-view")
@@ -25,23 +27,28 @@ const saveTodo = (text) => {
     checkBtn.classList.add("check-btn")
     const removeBtn = document.createElement("img")
     removeBtn.classList.add("remove-btn")
+    const editBtn = document.createElement("img")
+    editBtn.classList.add("edit-btn")
 
 
     checkBtn.src = "imgs/incon-check.svg"
     removeBtn.src = "imgs/icon-lixo.svg"
+    editBtn.src = "imgs/icon-edit.svg"
 
     icons.appendChild(checkBtn)
     icons.appendChild(removeBtn)
+    icons.appendChild(editBtn)
     taskView.appendChild(icons)
 
+    //IMPORTANDO TASK VIEW PARA O TO-DO
     generalTasks.appendChild(taskView)
 
     taskInput.value = ""//limpar input após o submit
     taskInput.focus() //foca no input após o submit
-
 }
-//Eventos
 
+//Eventos
+//Adicionar tarefa
 taskForm.addEventListener("submit", (element) => {
     element.preventDefault() //não quero enviar o form par ao back, ja que to trabalhando com o front
     const inputValue = taskInput.value
@@ -52,6 +59,29 @@ taskForm.addEventListener("submit", (element) => {
     }
 })
 
+//Pesquisar tarefa
+
+searchForm.addEventListener("submit", (element) => {
+    element.preventDefault()
+
+    const inputValue = searchInput.value
+    const taskViews = document.querySelectorAll(".task-view")
+
+    taskViews.forEach(taskView => {
+        const contentTask = taskView.querySelector(".content-task")
+
+        if (contentTask.innerText !== inputValue) {
+            taskView.style.display =  "none"
+        } else{
+            taskView.style.display = "block"
+        }
+    })
+
+    searchInput.value = ""//limpar input após o submit
+    searchInput.focus() //foca no input após o submit
+})
+
+
 
 document.addEventListener("click", (element) => {
     const targetEl = element.target
@@ -61,9 +91,15 @@ document.addEventListener("click", (element) => {
 
     if (targetEl.classList.contains("check-btn")) {
         generalTasksDone.appendChild(parentEl)
-    } 
-    
-    if (targetEl.classList.contains("remove-btn")) {
+    } else if (targetEl.classList.contains("remove-btn")) {
         parentEl.remove()
+    } else if (targetEl.classList.contains("edit-btn")){
+        const contentTask = parentEl.querySelector(".content-task")
+        const newText = prompt("Edite sua tarefa", contentTask.innerText)
+
+        if (newText !== null) {
+            contentTask.innerText = newText.trim() !== "" ? newText : contentTask.innerText
+        }
     }
 } )
+
